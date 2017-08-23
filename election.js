d7dee5f8c475718753bcb43ae7665356e5d01cb8
AlB <albert.burc@gmail.com>
2017-08-23 16:13:19 -0400

    finished displaying information through ajax request

--- election.js
+++ election.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
 // Imagination!
   $.ajax({
-    url: 'https://bb-election-api.herokuapp.com/';
-    method: 'GET';
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
     dataType: 'json'
-  }).done(function (data) {
-
+  }).done(function (responseData) {
+      for (var i = 0; i < responseData.candidates.length; i++) {
+        var newLi = document.createElement('li');
+        newLi.innerText = responseData.candidates[i].name + ' has ' + responseData.candidates[i].votes + ' votes ' ;
+        $('#bikbot').append(newLi);
+      }
   })
-
-});
+})
