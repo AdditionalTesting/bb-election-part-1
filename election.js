08a72a60563ffab522176ccbd9b7a1771248dfde
AlB <albert.burc@gmail.com>
2017-08-23 15:44:46 -0400

    added ajax request to the predefined API

--- election.js
+++ election.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
+// Imagination!
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/';
+    method: 'GET';
+    dataType: 'json'
+  }).done(function (data) {
 
-  // Imagination!
+  })
 
 });
